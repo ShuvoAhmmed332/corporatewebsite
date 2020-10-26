753669e84301d46cdb83df3cb08b3d64fe518afe
ShuvoAhmmed332 <70960249+ShuvoAhmmed332@users.noreply.github.com>
2020-10-26 09:53:14 -0700

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -10,7 +10,7 @@
         this.toRotate = toRotate;
         this.el = el;
         this.loopNum = 0;
-        this.period = parseInt(period, 10) || 2000;
+        this.period = parseInt(period, 3) || 2000;
         this.txt = '';
         this.tick();
         this.isDeleting = false;
