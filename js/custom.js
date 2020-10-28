770238143b66a12b458fb27270b5d3e1b49fb0c9
ShuvoAhmmed332 <70960249+ShuvoAhmmed332@users.noreply.github.com>
2020-10-28 08:19:23 -0700

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -1,71 +1,11 @@
-  $('.counter').counterUp({
+      
+       // Counter plugin:
+			$('.counter').counterUp({
 			delay: 10,
 			time: 1000
 			});
-
-
-
- /* ==== isotope ===== */
-
- var TxtType = function(el, toRotate, period) {
-        this.toRotate = toRotate;
-        this.el = el;
-        this.loopNum = 0;
-        this.period = parseInt(period, 3) || 2000;
-        this.txt = '';
-        this.tick();
-        this.isDeleting = false;
-    };
-
-    TxtType.prototype.tick = function() {
-        var i = this.loopNum % this.toRotate.length;
-        var fullTxt = this.toRotate[i];
-
-        if (this.isDeleting) {
-        this.txt = fullTxt.substring(0, this.txt.length - 1);
-        } else {
-        this.txt = fullTxt.substring(0, this.txt.length + 1);
-        }
-
-        this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
-
-        var that = this;
-        var delta = 200 - Math.random() * 100;
-
-        if (this.isDeleting) { delta /= 2; }
-
-        if (!this.isDeleting && this.txt === fullTxt) {
-        delta = this.period;
-        this.isDeleting = true;
-        } else if (this.isDeleting && this.txt === '') {
-        this.isDeleting = false;
-        this.loopNum++;
-        delta = 500;
-        }
-
-        setTimeout(function() {
-        that.tick();
-        }, delta);
-    };
-
-    window.onload = function() {
-        var elements = document.getElementsByClassName('typewrite');
-        for (var i=0; i<elements.length; i++) {
-            var toRotate = elements[i].getAttribute('data-type');
-            var period = elements[i].getAttribute('data-period');
-            if (toRotate) {
-              new TxtType(elements[i], JSON.parse(toRotate), period);
-            }
-        }
-        // INJECT CSS
-        var css = document.createElement("style");
-        css.type = "text/css";
-        css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
-        document.body.appendChild(css);
-    };
-  
-
-  /* ==== top to button ===== */
+	
+  // top to button plugin:
 
 $(document).ready(function(){
 
@@ -96,35 +36,7 @@ $('html').animate({scrollTop: 0},2000);
 
 });
 
-/* ==== carousel ===== */
-
-$('#logocarousel').owlCarousel({
-   		items:4,
-		loop:true,
-		margin:4,
-		autoplay:true,
-		autoplayTimeout:2000,
-		responsiveClass:true,
-		responsive:{
-			0:{
-			items:1,
-			},
-			576:{
-			items:2,
-			},
-			768:{
-			items:3,
-			},
-			1000:{
-			items:4,
-			},
-			1300:{
-			items:5,
-			dots:true,
-			}
-		}
-	})
-	   /* ==== smooth scrool ===== */
+// smooth scrool:
     $(document).ready(function(){
   // Add smooth scrolling to all links
   $("a").on('click', function(event) {
@@ -138,7 +50,7 @@ $('#logocarousel').owlCarousel({
       var hash = this.hash;
 
       // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (600) specifies the number of milliseconds it takes to scroll to the specified area
+      // The optional number (300) specifies the number of milliseconds it takes to scroll to the specified area
       $('html, body').animate({
         scrollTop: $(hash).offset().top
       }, 800, function(){
@@ -149,3 +61,4 @@ $('#logocarousel').owlCarousel({
     } // End if
   });
 });
+
